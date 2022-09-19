58949adcf2b0e970f99cd9f6c5f46eea1a0f6712
fleiscjl <113860663+fleiscjl@users.noreply.github.com>
2022-09-19 13:09:10 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+     int intOperandA = 8;
+     int intOperandB = 2;
+     int intProduct = 16;
+     int intDifference = 6;
+     int intQuotient = 4;
+     int intModulo = 0;
+     int intSum = 10;
+
+     double doubleOperandA = 1.50;
+     double doubleOperandB = 1.25;
+     double doubleSum = 2.75;
+     double doubleProduct = 1.875;
+     double doubleDifference = .25;
+     double doubleQuotient = 1.2;
+
+     intSum = intOperandA + intOperandB; // Assignment that uses the arithmentic operator
+     intProduct = intOperandA * intOperandB; // Assignment that uses the arithmentic operator
+     intDifference = intOperandA - intOperandB; // Assignment that uses the arithmentic operator
+     intQuotient = intOperandA / intOperandB; // Assignment that uses the arithmentic operator
+     System.out.println("The sum using ints of " + intOperandA + "" + intOperandB + " is " + intSum);
+     System.out.println("The product using ints of " + intOperandA + "" + intOperandB + " is " + intProduct);
+     System.out.println("The difference using ints of " + intOperandA + "" + intOperandB + " is " + intDifference);
+     System.out.println("The quotient using ints of " + intOperandA + "" + intOperandB + " is " + intQuotient);
+
+     doubleSum = doubleOperandA + doubleOperandB; // Assignment that uses the arithmentic operator
+     doubleDifference = doubleOperandA - doubleOperandA; // Assignment that uses the arithmentic operator
+     doubleProduct = doubleOperandA * doubleOperandB; // Assignment that uses the arithmentic operator
+     doubleQuotient = doubleOperandA / doubleOperandB; // Assignment that uses the arithmentic operator
+     System.out.println("The sum using ints of " + doubleOperandA + "" + doubleOperandB + " is " + doubleSum);
+     System.out.println("The product using ints of " + doubleOperandA + "" + doubleOperandB + " is " + doubleProduct);
+     System.out.println("The difference using ints of " + doubleOperandA + "" + doubleOperandB+ " is " + doubleDifference);
+     System.out.println("The quotient using ints of " + doubleOperandA + "" + doubleOperandB + " is " + doubleQuotient);
+
+     boolean
+     String birthMonth = "April";
+     int intNumKids = 3;
+     String firstName = "Justin";
+     String lastName = "Fleischman";
+     double doubleGasCost = 3.75;
+     double doubleShoeSize = 10.5;
+     int intFavNumber = 7;
+     System.out.println("Your shoe size is " + doubleShoeSize);
+     System.out.println("Your favorite number is " + intFavNumber);
+     System.out.println("Your cost per gallon is " + doubleGasCost);
+     System.out.println("Your full name is " + firstName + " " + lastName);
+     System.out.println("The number of kids in your family is" + " " + intNumKids);
+     System.out.println("Your birth month is " + birthMonth);
+     }
+
     }
-}
